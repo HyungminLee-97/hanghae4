bc813fd809267b3ce08b0c9f9492dc2c146261ac
seojuhye <seojuhye@seojuhyeui-MacBookAir.local>
2023-08-02 21:42:49 +0900

    commitM

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ $("#send").click(function () {
       alert("댓글 작성이 완료되었습니다.");
     });
 });
+
+function showPopup1() {
+  window.open("./about1.html","INTRODUCE MYSELF", "width=500, height=500, left=100, top=50")
+}
+
+function showPopup2() {
+  window.open("./about2.html","INTRODUCE MYSELF", "width=500, height=500, left=100, top=50")
+}
+
+function showPopup3() {
+  window.open("./about3.html","INTRODUCE MYSELF", "width=500, height=500, left=100, top=50")
+}
