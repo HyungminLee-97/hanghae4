e6c7c3ed992c45f8fdb8fa9df4cabaf8b185b5e4
‘dlgudals1744@nate.com’ <dlgudals1744@nate.com>
2023-08-03 21:19:47 +0900

    서버 작업

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, function () {
+  console.log("3000번 포트가 열림");
+});
+
+// index 페이지 출력
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// 서주헤 profile about1
+app.get("/about1", function (req, res) {
+  res.sendFile(__dirname + "/about1.html");
+});
+
+// 이형민 profile about2
+app.get("/about2", function (req, res) {
+  res.sendFile(__dirname + "/about2.html");
+});
+
+// 오윤성 profile about3
+app.get("/about3", function (req, res) {
+  res.sendFile(__dirname + "/about3.html");
+});
