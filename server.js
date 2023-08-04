24abf4fa5f55bc682a37aad84094a02c01bc7134
‘dlgudals1744@nate.com’ <dlgudals1744@nate.com>
2023-08-04 20:24:52 +0900

    댓글 db 연결 완료

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express");
-const fs = require('fs');
+const fs = require("fs");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-  console.log("3000번 포트가 열림");
-});
+const MongoClient = require("mongodb").MongoClient;
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.gq7c239.mongodb.net/?retryWrites=true&w=majority",
+  function (error, client) {
+    if (error) return console.log(error);
+
+    //mongodb 내에 hanghae4라는 databse 폴더에 연결
+    db = client.db("hanghae4");
+
+    // db.collection('post').insertOne({이름: . }, function(error, result){
+    //   console.log('작성 완료')
+    // });
+
+    //서버띄우는 코드
+    app.listen(3000, function () {
+      console.log("3000번 포트가 열림");
+    });
+  }
+);
 
 // index 페이지 출력
 app.get("/", function (req, res) {
@@ -24,6 +42,12 @@ app.get("/:id", function (req, res) {
 app.post("/add", function (req, res) {
   res.send("댓글이 작성되었습니다.");
   console.log(req.body);
+  db.collection("post").insertOne(
+    { 이름: req.body.name, 댓글: req.body.comment, 암호: req.body.password },
+    function (error, result) {
+      console.log("댓글 작성 완료");
+    }
+  );
 });
 
 //css,img,js 적용
